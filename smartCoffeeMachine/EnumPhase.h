12fe8231e30e6bd9159534c41b174f9491f769af
Luca Valentini <luca.valentini15@studio.unibo.it>
2018-11-13 23:08:43 +0100

    delete on_ascent and on_discent in enumPhase

--- smartCoffeeMachine/EnumPhase.h
+++ smartCoffeeMachine/EnumPhase.h
@@ -1,8 +1,6 @@
 enum class EnumPhase{
   SLEEP,
   ON,         //STATE OF ON PHASE
-  ON_ASCENT,  //FROM SLEEP TO ON //da togliere
-  ON_DISCENT, //FROM READY TO SLEEP // da togliere
   READY,
   BUSY,
   DONE,
